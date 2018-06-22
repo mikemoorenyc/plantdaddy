58ec9973a4bd341697baefb64e4241ecc1b2dbdf
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-22 13:43:12 -0400

    Create is_user_logged_in.php

---
+++ global_functions/is_user_logged_in.php
@@ -0,0 +1,16 @@
+<?php
+function is_user_logged_in() {
+  
+  if($_SESSION['logged_in'] && $_SESSION['current_user']) {
+   return true; 
+  }
+  
+  $remembered = verify_remember_me();
+  
+  if($remembered) {
+    return true;
+  } else {
+    return false;
+  }
+  return false;
+}
