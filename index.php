7e24a2e46ea93e86ac2fed1d5bd3477bd736925b
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-25 11:34:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-echo file_get_contents("http://localhost:8080/");
+require "/header.php";
+
+
+$HTML = file_get_contents("http://localhost:8080/");
+
+$initInfo = [];
+
+$initInfo['loggedIn'] = is_user_logged_in();
+
+
+
+$initJS = "<script>var INITINFO = ".json_encode($initInfo)."; </script>";
+
+$HTML = str_replace("***INIT PACKAGE GOES HERE***",$initJS,$HTML);
+
+echo $HTML;
+
 
 
  ?>
