79cd93e6824be61b196414c686540eb5b355595a
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-25 14:53:40 -0400

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,40 @@
 import { h, Component } from 'preact';
-import { Router } from 'preact-router';
+import { Router, route } from 'preact-router';
 
 import Header from './header';
 import Home from './home';
 import Profile from './profile';
 
 export default class App extends Component {
+	constructor(props) {
+		super();
+    this.state = {
+     isLoggedIn: props.isLoggedIn 
+    }
+		
+	}
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
 	 */
 	handleRoute = e => {
 		this.currentUrl = e.url;
+    if(!this.state.isLoggedIn) {
+     route('/login/', true) 
+    }
 	};
+
+	componentWillMount() {
+		if(!this.state.isLoggedIn) {
+      route('/login/', true) 
+    }
+	}
 
 	render() {
 		return (
 			<div id="app">
 				<Header />
-				<Router onChange={this.handleRoute}>
+				<Router onChange={this.handleRoute.bind(this)}>
 					<Home path="/" />
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
