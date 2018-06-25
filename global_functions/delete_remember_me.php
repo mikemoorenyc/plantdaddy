8f0380900c99e9fde8ce6da99c781c86066b5029
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-25 11:23:17 -0400

    Update delete_remember_me.php

--- global_functions/delete_remember_me.php
+++ global_functions/delete_remember_me.php
@@ -1,11 +1,11 @@
 <?php
 function delete_remember_me() {
   global $db_conn;
-   $credentials = json_decode($_COOKIE['plantdaddy_remember_me'],true);
+   $credentials = json_decode($_COOKIE[REMEMBER_ME_NAME],true);
    $sql =  "DELETE FROM tokens WHERE selector=".intval($credentials['selector']);
    $deleted_token =  mysqli_query($db_conn, $sql);
    if($deleted_token) {
-     setcookie("ur_fat_remember_me", "", time() - 3600,'/');
+     setcookie(REMEMBER_ME_NAME, "", time() - 3600,'/');
      return true;
    } else {
      return false;
