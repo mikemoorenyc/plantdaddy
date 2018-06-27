b8168b792e90680ce1924fd432bf9fbe11891390
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-27 10:48:38 -0400

    Update FormSection.jsx

--- src/components/EditAccount/FormSection.jsx
+++ src/components/EditAccount/FormSection.jsx
@@ -1,1 +1,19 @@
 import { h} from 'preact';
+import {linkstate} from "linkstate";
+
+export default function(p) {
+	return(
+		<div class="formRow">
+			<label for={p.labelShort}>{p.label}</label><br/>
+				<input required type={p.type} value={p.value} name={p.labelShort} onChange={linkstate(this,p.labelShort)} />
+		</div>
+		
+		
+		
+		
+	)
+		
+	
+	
+	
+}
