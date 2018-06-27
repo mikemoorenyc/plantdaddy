a879a78c288b2c4221161f375730a99965db8189
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-27 16:07:13 -0400

    Update get_user_by_id.php

--- global_functions/get_user_by_id.php
+++ global_functions/get_user_by_id.php
@@ -1,2 +1,19 @@
 <?php
 
+function get_user_by_id($id=$_SESSION['current_user']['id']) {
+	global $db_conn;
+	$get_user =  "SELECT * FROM users WHERE `id` = '".$db_conn->real_escape_string($id)."' LIMIT 1";
+	$user = $db_conn->query($get_user);
+  if(!$user) {
+    return false;
+  }
+  if($user->num_rows < 1) {
+    return false;
+  }
+  return $user->fetch_assoc();
+	
+	
+	
+}
+
+?>
