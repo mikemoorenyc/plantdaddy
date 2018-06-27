9d7ec2642ba8187105a3aad78b4ca6a9c475cfee
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-27 14:26:50 -0400

    Update api-router.php

--- endpoints/api-router.php
+++ endpoints/api-router.php
@@ -1,1 +1,19 @@
+<?php
 
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+switch($url) {
+    case '/create-account/':
+        require 'create-account.php';
+        break;
+
+
+    default:
+			http_response_code(404);
+			die();
+				
+}
+
+
+?>
