1786604c94e7952755db79b88f090edace9aaef8
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-28 11:06:10 -0400

    Update UserContainer.js

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -3,10 +3,13 @@ import {Container } from "unstated";
 class UserContainer extends Container{
   state = {
     isLoggedIn: INITINFO.isLoggedIn,
-		userProfile: INITINFO.userProfile
+		userProfile: INITINFO.userProfile,
+		login_noonce: INITINFO.login_noonce
   };
-recieveUserProfile(profile) {
-	this.setState({user
+recieveNewStateItem(key, state) {
+	let sObj = {};
+	sObj['key'] = state;
+	this.setState(sObj);
 }
 
   
