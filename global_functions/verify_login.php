11ad3130def6f75d14bb7706b61ed9c75057397a
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-28 11:33:32 -0400

    Create verify_login.php

---
+++ global_functions/verify_login.php
@@ -0,0 +1,20 @@
+<?php
+function verify_login($email=null,$password=null) {
+  global $db_conn;
+  if(!$email || !$password) {
+    return false;
+  }
+    $get_user =  "SELECT * FROM users WHERE `email` = '".$db_conn->real_escape_string($email)."' LIMIT 1";
+  $user = $db_conn->query($get_user);
+  if($user->num_rows < 1 || !$user) {
+    return false;
+  }
+  $user = $user->fetch_assoc();
+  //CHECK PASSWORD
+$pass_pass = password_verify(base64_encode(hash('sha256', $password, true)),$user['password']);
+  if(!$pass_pass) {
+    return false;
+  }
+  return login_user($user['id']);
+}
+ ?>
