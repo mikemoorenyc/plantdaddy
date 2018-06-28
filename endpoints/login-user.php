c0cc685b39b8e0e6ba56daf8df8fce47cda33db5
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-28 11:32:11 -0400

    Update login-user.php

--- endpoints/login-user.php
+++ endpoints/login-user.php
@@ -1,1 +1,14 @@
+<?php
+require_once "/header.php";
+require_once "endpoint-header.php";
 
+if($_SESSION['login_noonce'] !== $_POST['login_noonce']) {
+  $_SESSION['login_noonce'] = generate_noonce();
+	$error = array(
+		"msg" => "Bad Noonce",
+		"new_login_noonce" => $_SESSION['login_noonce']
+	);
+	errorResponse(400, $error);
+}
+
+$logged_in_user = verify_login($_POST['email'],$_POST['pass']);
