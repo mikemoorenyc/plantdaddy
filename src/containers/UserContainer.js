2a36b2089b47b6b41dab468e6cbf8202093b7c78
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-28 09:04:30 -0400

    Update UserContainer.js

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -2,14 +2,8 @@ import {Container } from "unstated";
 
 class UserContainer extends Container{
   state = {
-    isLoggedIn: 
+    isLoggedIn: INITINFO.isLoggedIn
   };
 
-  increment() {
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  decrement() {
-    this.setState({ count: this.state.count - 1 });
-  }
+  
 }
