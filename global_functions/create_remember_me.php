7a842d672012d41ba54ad5b5166c2bfb1363f4f9
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-28 12:32:14 -0400

    Update create_remember_me.php

--- global_functions/create_remember_me.php
+++ global_functions/create_remember_me.php
@@ -9,6 +9,15 @@ function create_remember_me($id) {
   $plain_token = generate_noonce();
   $validator = hash('sha256', $plain_token, true);
   $user_id = intval($id);
-
+	$expires = strtotime("+6 month");
+	$insert_token = "INSERT INTO tokens (selector, hashedValidator, user_id, expires)
+    VALUES ('$selector','$validator','$user_id','$expires')";
+  $db_token = $dbconn->mysqli_query($db_conn, $insert_token);
+  if($db_token) {
+    setcookie("ur_fat_remember_me", json_encode(array("selector" => $selector , "validator" => $plain_token)),$expires, '/');
+    return true;
+  } else {
+    return false;
+  }
 }
 
