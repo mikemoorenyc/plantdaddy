ec3d271bb75fd1b5f60246a3924db85bec07627a
Mike Moore <tigerhollowpoint@gmail.com>
2018-06-28 21:30:04 -0400

    added things

---
+++ global_functions/verify_reset_token.php
@@ -0,0 +1,22 @@
+<?php
+verify_reset_token($token) {
+  global $db_conn;
+  if(!$token) {return false;}
+
+  $get_user =  "SELECT * FROM users WHERE `reset_token` = '".$db_conn->real_escape_string($token)."' LIMIT 1";
+  $user = $db_conn->query($get_user);
+  if(!$user) {return false;}
+  if($user->num_rows < 1) {return false;}
+  $user->fetch_assoc();
+  //RESET EVERTHING
+  $id = $user['id'];
+  $expires = $user["reset_expires"]
+  $sql = "UPDATE users SET reset_token=null, reset_expires=0  WHERE id=".$id;
+  $delete_token = mysqli_query($db_conn, $sql);
+
+  if( $expires < time()) {return false;}
+
+  return true;
+}
+
+ ?>
