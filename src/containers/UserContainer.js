4621b56653ac1a38e95720c9aec5f34e5bee4ca5
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-03 08:54:26 -0400

    Update UserContainer.js

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -1,23 +1,10 @@
-import { Container } from 'unstated'
+import { Container } from 'unstated';
 import { h, Component } from 'preact';
 
 export default class UserContainer extends Container{
-  constructor() {
-    super();
-    this.state = {
-      isLoggedIn: INITINFO.isLoggedIn,
-  		userProfile: INITINFO.userProfile,
-  		login_noonce: INITINFO.login_noonce
-    }
-  }
-
-
-  /*
-recieveNewStateItem(key, state) {
-	let sObj = {};
-	sObj['key'] = state;
-	this.setState(sObj);
-};
-*/
+  state = {
+	  isLoggedIn: false
+	  
+  };
 
 }
