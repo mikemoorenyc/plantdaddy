a1b26abe738cbf8800c416c7d8e4bc37a412eaaf
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-10 17:14:04 -0400

    Update code-reset-password.php

--- endpoints/code-reset-password.php
+++ endpoints/code-reset-password.php
@@ -6,24 +6,19 @@ require_once "endpoint-header.php";
 $token_verified = verify_reset_token($response['reset_token']);
 if($token_verified !== true) {
 
-  errorResponse(400, array("msg"=> (DEV_NEV) ? $token_verified : "Bad Token", "error_code" => "bad_reset_token"));
+  errorResponse(400, "bad_reset_token");
 }
 
 if($_SESSION['login_noonce'] !== $response['login_noonce']) {
-	errorResponse(400, array("msg"=> "Bad Noonce", "error_code" => "bad_noonce"));
+	errorResponse(400, "bad_noonce");
 }
 
-$mailError = array(
-	"msg" => "Bad Email",
-	"error_code" => "bad_email",
-  "success" => false,
-	"error_info" => $response['email']
-);
+
 if (!get_user_by_email($response['email']) ){
-	errorResponse(400, $mailError);
+	errorResponse(400, "bad_email");
 }
 if($response['password'] !== $response['password_2']) {
-  errorResponse(400, array("msg" => "Passwords do not match", "error_code"=>"pw_mismatch"));
+  errorResponse(400, "pw_mismatch");
 }
 
 $new_password = pw_hasher($response['password']);
@@ -41,11 +36,8 @@ $updated_pass = update_item($update_package);
 
 
 if(!$updated_pass) {
-  $error = array(
-    "msg" => "Couldn't Update",
-    "success" => false
-  );
-  errorResponse(500, $error);
+
+  errorResponse(500, "update_error");
 }
 echo json_encode(array(
   "msg" => "Password updated Successfully",
