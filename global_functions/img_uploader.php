6e29ab68e0af2b872299ca33abd70b88fae73996
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-10 09:36:45 -0400

    Update img_uploader.php

--- global_functions/img_uploader.php
+++ global_functions/img_uploader.php
@@ -1,17 +1,20 @@
 <?php
 
 function img_uploader($base64, $filename) {
-	$assets_route = $_SERVER['DOCUMENT_ROOT'] ."/assets/user_images/";
-	$name = $assets_router.str_replace(['.jpg.','.png','.gif','.jpeg'],'',strtolower($filename)).'.jpg';
+	$name = "/assets/user_images/".str_replace(['.jpg.','.png','.gif','.jpeg'],'',strtolower($filename)).'.jpg';
+	$asset_route = $_SERVER['DOCUMENT_ROOT'] .$name;
 	
-	 $data = explode( ',', $base64 );
+	$data = explode( ',', $base64 );
 	if($data >1){return false;}
 	
 	$img = imagecreatefromstring($data[1]);
+	if(!$img) {
+		return false;
+	}
 	
 	$scaled = imagescale($img, 1000); 
 	
-	$saved = imagejpeg($image, $name, 65);
+	$saved = imagejpeg($image, $asset_route, 65);
 	if(!$saved) {return false;}
 	
 	return $name;
