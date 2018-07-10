ab6174406020922de2633c1c7c867902c0db9ba4
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-10 09:52:21 -0400

    Update insert_item.php

--- global_functions/insert_item.php
+++ global_functions/insert_item.php
@@ -1,6 +1,20 @@
 <?php
-insert_item() {
-
-
+insert_item($table, $insert_array) {
+	global $db_conn;
+	if(!is_array($insert_array || !$table) ) {
+		return false;
+	}
+	$safe_values = [];
+	foreach($insert_array as $k =>$v) {
+		$safe_valules[$k] = $db_conn->real_escape_string($v);
+	}
+	$insert_values = implode(", ",$safe_values);
+	$insert_keys = implode(", ", array_keys($safe_values));
+	$insert_db = "INSERT INTO $table ($insert_keys) VALUES ($insert_values)";
+	$insert_item = mysqli_query($db_conn, $insert_db);
+	if(!$insert_item) {
+		return false;
+	}
+	return mysqli_insert_id($db_conn);
 
 }
