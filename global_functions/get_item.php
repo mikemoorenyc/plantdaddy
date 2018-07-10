2d64dda1d22c688144baa523df76875d3f66189f
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-10 14:25:16 -0400

    Update get_item.php

--- global_functions/get_item.php
+++ global_functions/get_item.php
@@ -1,1 +1,30 @@
 
+<?php
+get_items($table, $values="*", $selector_key, $selector_value, $limit=1 ) {
+	if(!$selector_key || !$selector_value) {
+		return false;
+	}
+	global $db_conn;
+	$safe_values = [];
+	if(is_array($values)) {
+		foreach($values as $v) {
+			$safe_values = $db_conn->real_escape_string($v);
+		}
+		$safe_values = implode(", ", $safe_values);
+	} else {
+		$safe_values = $db_conn->real_escape_string($value);
+	}
+	$safe_key = $db_conn->real_escape_string($selector_key);
+	$safe_selector = $db_conn->real_escape_string($selector_value);
+	$safe_limit = intval($limit);
+	
+	$get_items =  "SELECT $safe_values FROM $table WHERE `$safe_key` = '$safe_selector' LIMIT ".$safe_limit;
+	$items = $db_conn->query($get_user);
+	if(!$items) {
+    return false;
+  }
+  if($items->num_rows < 1) {
+    return false;
+  }
+  return $user->fetch_assoc();
+}
