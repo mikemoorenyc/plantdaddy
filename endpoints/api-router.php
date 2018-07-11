69d15d1de891e9f2d57c7662c75261447967dab8
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-11 13:03:54 -0400

    Update api-router.php

--- endpoints/api-router.php
+++ endpoints/api-router.php
@@ -1,23 +1,25 @@
 <?php
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$url = str_replace("/endpoints","",$url);
+$url_sections = explode('/',$url);
+$url_sections = array_filter($url_sections);
 
 
-$url = str_replace('/endpoints','',parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-
-switch($url) {
-    case '/create-account/':
+switch($url_sections[0]) {
+    case 'create-account':
         require 'create-account.php';
         break;
-	case "/login-user/":
+	case "login-user":
 		require "login-user.php";
 		break;
-	case "/require-reset/":
+	case "require-reset":
 		require "code-require-reset.php";
 		break;
-  case "/reset-password/":
+  case "reset-password":
 		require "code-reset-password.php";
 		break;
-  case "/logout-user/":
+  case "logout-user":
   		require "code-logout-user.php";
   		break;
 
