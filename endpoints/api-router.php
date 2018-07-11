8e45613fb02ec45bfa87e18507b05ce78c3e02a1
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-11 16:38:47 -0400

    Update api-router.php

--- endpoints/api-router.php
+++ endpoints/api-router.php
@@ -4,6 +4,12 @@ $url = str_replace("/endpoints","",$url);
 $url_sections = explode('/',$url);
 $url_sections = array_filter($url_sections);
 
+$open_routes = ['create-account','login-user','require-reset','reset-password'];
+
+if(!$_SESSION['logged_in'] && !in_array($url_sections[0],$open_routes)) {
+	http_response_code(403);
+	die();
+}
 
 
 switch($url_sections[0]) {
@@ -19,9 +25,7 @@ switch($url_sections[0]) {
   case "reset-password":
 		require "code-reset-password.php";
 		break;
-  case "logout-user":
-  		require "code-logout-user.php";
-  		break;
+
 
 
     default:
