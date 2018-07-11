8b09e0a78bfade12be17f8306d30d5f45878b36a
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-11 11:16:09 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,1 +1,28 @@
+import { h } from 'preact';
 
+
+
+export default function(p) {
+	let title = p.title || "PlantDaddy" ;
+	return (
+	<div id="app">
+		<div class="header">
+			<div class="left-side">
+				{p.headerLeft}		
+			</div>
+			<h1>{title}</h1>
+			<div class="right-side">
+				{p.headerRight}
+			</div>
+		</div>
+			<div class="content">
+				{p.children}
+			</div>
+	</div>	
+		
+	
+	)
+
+	
+	
+}
