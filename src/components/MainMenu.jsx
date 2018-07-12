a28b000ff52330fa4741b3a4ad0dfbaf3ee7eeac
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-12 10:00:05 -0400

    Update MainMenu.jsx

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,1 +1,20 @@
+import {h} from "preact";
 
+
+
+export default function(p) {
+	return(
+	<div class="menu">
+		<ul>
+			<li><a href="/account/">View Your Account</a></li>
+			<li><a native "/logout/">Logout</a></li>
+			
+			
+	</div>
+	
+	
+	)
+	
+	
+	
+}
