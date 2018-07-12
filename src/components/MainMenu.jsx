42d35cec94955fd6f82742fe29477284ff0a55fb
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-12 15:45:29 -0400

    Update MainMenu.jsx

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,17 +1,27 @@
 import {h} from "preact";
-
+import { Subscribe } from 'unstated';
+import UserContainer from "../containers/UserContainer.js";
 
 
 export default function(p) {
 	return(
-	<div class="menu">
+<Subscribe to={[UserContainer]}>
+	{function(user) {
+	return(
+		<div class="menu">
+		<a href="/account/">
+				
+		</a>
 		<ul>
 			<li><a href="/account/">View Your Account</a></li>
 			<li><a native "/logout/">Logout</a></li>
 			</ul>	
 			
 	</div>
-	
+		
+		)
+}}
+</Subscribe>
 	
 	)
 	
