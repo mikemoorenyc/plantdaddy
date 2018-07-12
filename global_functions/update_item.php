73394cc10fc1f0b63933ef3f83d97f3d42a9359f
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-12 13:30:16 -0400

    Update update_item.php

--- global_functions/update_item.php
+++ global_functions/update_item.php
@@ -1,13 +1,16 @@
 <?php
 function update_item($p) {
   if(!$p || !is_array($p)) {return false;}
-  global $db_conn;
-  //MAKE SURE EVERYTHING IS THERE
-  $values = ['update_array',"db","selector_key","selector_value"];
+	global $db_conn;
+  $values = ['update_values',"db","selector_key","selector_value"];
+	
   if(!empty(array_diff($values,array_keys($p)))){return false;}
 	if(!is_array($p['update_array'] )) {return false;}
+	
 	$update_array = $p['update_array'];
+	
 	$update_array['date_modified'] = $update_array['date_modified'] ?: time();
+	
 	$update_array['modified_by'] = $update_array['modified_by'] ?: $_SESSION['user']['id'];
 
 	if(!$update_array['modified_by']) {
@@ -15,6 +18,7 @@ function update_item($p) {
 	}
 
   $db = $db_conn->real_escape_string($p["db"]);
+	
 	$safe_values = [];
 	foreach($update_array as $k => $v) {
 		$value = (is_int($v)) ? intval($v) : "'".$db_conn->real_escape_string($v)."'";
@@ -23,25 +27,42 @@ function update_item($p) {
 	}
 	$update_string = implode(", ",$safe_values);
 
-  $selector_key = $db_conn->real_escape_string($p['selector_key']);
-  $selector_value = (is_int($p['selector_value'])) ? intval($p['selector_value']) : "'".$db_conn->real_escape_string($p['selector_value'])."'";
+  
+	$selector_key = $db_conn->real_escape_string($p['selector_key']);
+  
+	$selector_value = (is_int($p['selector_value'])) ? intval($p['selector_value']) : "'".$db_conn->real_escape_string($p['selector_value'])."'";
+	
+	//Check if value will update multiple rows
+	$rows = get_items(array(
+		"db" => $db,
+		"selector_key" => $selector_key,
+		"selector_value" => $selector_value
+	));
+	
+	if(count($rows) !== 1) {return false;}
 
-  $sql = "UPDATE $db SET $update_string WHERE `$selector_key` = $selector_value";
-
+	
+	
+  $sql = (
+	"UPDATE 
+		$db 
+	SET 
+		$update_string 
+	WHERE 
+		`$selector_key` = $selector_value"
+	);
 
   $update_item = mysqli_query($db_conn, $sql);
+	
   if(!$update_item){return false;}
-
-
-  $get_item_sql = "SELECT id FROM $db WHERE `$selector_key` = $selector_value LIMIT 1";
-	$get_item = $db_conn->query($get_item_sql);
-  if(!$get_item) {
-    return false;
-  }
-  if($get_item->num_rows < 1) {
-    return false;
-  }
-  $item = $get_item->fetch_assoc();
+	
+	$item = get_items(array(
+		"db" => $db,
+		"columns" => "id",
+		"limit" => 1,
+		"selector_key" => $selector_key,
+		"selector_value" => $selector_value
+	));
 
 	return $item['id'];
 
