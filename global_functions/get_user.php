87d5429d461e403d4420e0664c5835c1d21c34ff
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-12 09:33:56 -0400

    Update get_user.php

--- global_functions/get_user.php
+++ global_functions/get_user.php
@@ -6,7 +6,13 @@ function get_user($key,$selector) {
     return 'bad parameters';
   }
   $columns = ['id','email','first_name',"color","telephone","photo_id","date_created","date_modified"];
-  $user =  get_items("users",$columns, $key, $selector);
+  $user =  get_items(array(
+		"limit" => 1,
+		"table" => users.
+		"selector_key" => $key,
+		"selector_value" => $selector,
+		"columns" => $columns
+	));
   if(!$user) {return false;}
 
   $user['photo_url'] = ($user['photo_id']) ? get_photo_by_id($user['photo_id']) : '';
