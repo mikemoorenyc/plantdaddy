3ba05c128081c8c456de805326dc8878a1abb3ca
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-17 16:25:44 -0400

    Update ChangePassword.jsx

--- src/components/EditAccount/ChangePassword.jsx
+++ src/components/EditAccount/ChangePassword.jsx
@@ -13,7 +13,8 @@ export default class extends Component {
 		this.state = {
 			current_password: '',
 			new_password: '',
-			new_password_2: ''
+			new_password_2: '',
+			status: "inputing"
 		}
 		this.submitForm = this.submitForm.bind(this);
 	}
@@ -27,17 +28,24 @@ export default class extends Component {
 			//No match
 			return false;
 		}
+		this.setState({status: "loading"});
 		fetch(sendPackage,"/endpoints/accounts/?change_password=1", "POST", this.successHandler );
 		
 		
 	}
-	successHandler(e) {
-		
+	successHandler(data) {
+		if(!data.success) {
+			//ERROR HANDLING
+			return false;
+		}
+		this.setState({status: "success"});
 		
 	}
 	
 	render(props,state) {
-		let disabledState = (state.password && state.new_password && state.new_password_2)  || false;
+		let disabledState = (state.password && state.new_password && state.new_password_2 && state.status == "inputing")  || false;
+		let disabledFields = (state.status !== "inputing" || state.status !== "server_error") || false;
+		let successMsg = (state.status == "success") ? <div>Password Changed</div> || null;
 		let sections = [
 			{
 				labelShort:"current_password",
@@ -60,6 +68,7 @@ export default class extends Component {
 					label={e.labelLong}
 					onInput={linkstate(this, e.labelShort)}
 					type={"password"}
+					disabled={disabledFields}
 				/>			
 			)
 			
@@ -70,7 +79,9 @@ export default class extends Component {
 						
 					{sections}
 					<button disabled={disabledState} type="submit">Change Password</button> 
+					{successMsg}
 				</form>
+				
 			</Layout>
 		
 		)
