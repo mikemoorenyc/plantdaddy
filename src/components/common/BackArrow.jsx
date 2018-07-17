1c58c178b8d48fcc4a034fbfec1e206529a757dd
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-17 09:44:02 -0400

    Update BackArrow.jsx

--- src/components/common/BackArrow.jsx
+++ src/components/common/BackArrow.jsx
@@ -2,10 +2,15 @@ import {h} from "preact";
 
 
 export default function(p) {
-  return(
-    <a href={p.href} native={p.native}>
-      Back
-    </a>
-  )
+	if(p.custom) {
+		return(
+    	<a href={p.href} native={p.native}>Back</a>
+  	)
+	}
+	return(
+		<button onClick={()=>(window.history.back())}>Back</button>
+	
+	)
+  
 
 }
