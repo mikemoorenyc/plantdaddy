b8030175e732117c6809dc2dc0524f9fa1789c61
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-17 09:32:44 -0400

    Update account.jsx

--- src/components/account/account.jsx
+++ src/components/account/account.jsx
@@ -1,9 +1,39 @@
-import {h} from "preact";
+import {Component, h} from "preact";
+
+import Layout from "../Layout";
+import ProfileImg from "../common/ProfileImage";
 
 
 
-export default function(p) {
-  return <div>
-    {p.id}
-  </div>
+export default class Account extends Component  {
+  constructor(props) {
+		super();
+		this.state = {
+			id : props.id || props.user.state.user.id,
+			loaded: false,
+			user: null
+		}
+		
+	}
+	componentWillMount() {
+		if(this.state.id === this.props.user.state.user.id) {
+			this.setState({user:this.props.user.state.user, loaded: true});
+			return false;
+		}
+	}
+	
+	
+	render(props,state) {
+		let editButton = (props.user.state.user.id == state.id ) ? <a href="/edit-account/">Edit</a> : null;
+		
+		return(
+				<Layout header={state.user.first_name} headerRight={editButton}>
+					<ProfileImg user={state.user} />
+					<h1>{state.user.first_name}</h1>
+					
+				</Layout>
+			
+			
+		);
+	}
 }
