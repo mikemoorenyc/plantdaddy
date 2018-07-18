2dfc4166af597ea9297661771db13c712edb77b0
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-18 13:32:38 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,39 +5,31 @@ import UserContainer from "../containers/LayoutContainer.js";
 import MainMenu from "./MainMenu.jsx";
 
 export default function(p) {
-	let title = p.title || "PlantDaddy" ;
-	return (
-<Subscribe to={[UserContainer,LayoutContainer]}>
-	{function(user,layout) {
-		let menu = (user.state.isLoggedIn) ? <MainMenu /> : null;
-		return(
-	<div id="app" className={(layout.state.menuOpen) ? "menu-open" : null}>
-		{menu}
-		<div class="main-content">
-			<div class="header">
-			<div class="left-side">
-				{p.headerLeft}
-			</div>
-			<h1>{title}</h1>
-			<div class="right-side">
-				{p.headerRight}
-			</div>
-		</div>
-			<div class="content">
-				{p.children}
-			</div>
-
-		</div>
-
-
-	</div>
-
-			)
-		}}
-</Subscribe>
-
-	)
-
-
-
+  let title = p.title || "PlantDaddy" ;
+  return (
+    <Subscribe to={[UserContainer,LayoutContainer]}>
+      {function(user,layout) {
+        let menu = (user.state.isLoggedIn) ? <MainMenu /> : null;
+        return(
+          <div id="app" className={(layout.state.menuOpen) ? "menu-open" : null}>
+            {menu}
+            <div class="main-content">
+              <div class="header">
+                <div class="left-side">
+                  {p.headerLeft}
+                </div>
+                <h1>{title}</h1>
+                <div class="right-side">
+                  {p.headerRight}
+                </div>
+              </div>
+              <div class="content">
+                {p.children}
+              </div>
+            </div>
+          </div>
+        )
+      }}
+    </Subscribe>
+  )
 }
