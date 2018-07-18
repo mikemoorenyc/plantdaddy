9418f13e0b795f226c9c381ea3a8711e6794371a
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-18 11:05:28 -0400

    Update MenuToggle.jsx

--- src/components/common/MenuToggle.jsx
+++ src/components/common/MenuToggle.jsx
@@ -1,1 +1,21 @@
+import {h} from "preact";
+import LayoutContainer from "../../containers/LayoutContainer";
+import {Subscribe} from "unstated";
 
+export default function(p)  {
+	return (
+		<Subscribe to={[LayoutContainer]}>
+			{layout => (
+				<button onClick={(e) => (e.preventDefault(); p.toggleMenu();)}>
+				Menu
+				</button>
+			)}
+			
+			
+		</Subscribe>
+		
+		
+		)
+	
+	
+}
