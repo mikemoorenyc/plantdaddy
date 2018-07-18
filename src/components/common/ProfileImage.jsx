eb1946b95cebaaa48a5de81872f9e81c0d560889
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-18 11:53:47 -0400

    Update ProfileImage.jsx

--- src/components/common/ProfileImage.jsx
+++ src/components/common/ProfileImage.jsx
@@ -1,15 +1,12 @@
 import {h} from "preact";
 
 export default function({user}) {
-	let color = user.color;
-	let interior = (user.photo_url)?
-			<img src={user.photo_url} alt={user.first_name} /> :
-			<span>{p.user.first_name.charAt(0)}</span>;
-
-	return(
-		<a style={{backgroundColor: {color}} href={`/account/${user.id}/`}>
-			{interior}
-		</a>
-	)
-
+  let color = user.color;
+  let interior = (user.photo_url)? <img src={user.photo_url} alt={user.first_name} /> : <span>{p.user.first_name.charAt(0)}</span>;
+  
+  return(
+    <a style={{backgroundColor: {color}} href={`/account/${user.id}/`}>
+      {interior}
+    </a>
+  )
 }
