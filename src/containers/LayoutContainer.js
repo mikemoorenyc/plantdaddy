4f9b083faf425d94b40e51cbaf71bb64ea5400b7
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-18 10:34:20 -0400

    Update LayoutContainer.js

--- src/containers/LayoutContainer.js
+++ src/containers/LayoutContainer.js
@@ -1,1 +1,17 @@
+import { Container } from 'unstated';
 
+
+export default class LayoutContainer extends Container {
+	state = {
+		menuOpen : false
+	}
+  toggleMenu() {
+   let current = this.state.menuOpen;
+   this.setState({
+    menuOpen (current) ? false : true
+   }) 
+  }
+	
+	
+	
+}
