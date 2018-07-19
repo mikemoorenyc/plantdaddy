af2e37ceda09c9779b9c88714b8070b01a30427e
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-19 13:20:25 -0400

    Update svgContainer.js

--- src/containers/svgContainer.js
+++ src/containers/svgContainer.js
@@ -1,1 +1,41 @@
-import
+import {Container} from "unstated";
+
+
+export default class SvgContainer extends Container {
+	state = {
+		svgs: {}
+	}
+	
+	getSvg(name) {
+		let iconName = name.replace('.svg','').toLowerCase();
+		if(this.state.svgs['iconName'].code) {
+			return false;
+		}
+		fetch(`/assets/icons/${iconName}.svg`)
+		.then(function(response) {
+			if(response.status > 299) {
+				throw {
+					success:false,
+					status : response.status,
+				}
+			} else {
+				return response.text();
+			}
+		})
+		.then(function(r){
+			let svgs = this.state.svgs;
+			svgs["iconName"] = {
+				code: r		
+			}
+			this.setState({svgs: svgs});
+			return false;
+  	}.bind(this))
+		.catch(function(error) {
+			return false;
+		})
+		
+		
+	}
+	
+	
+}
