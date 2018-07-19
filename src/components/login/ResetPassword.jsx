8b3265a22e010de7712d22074443cb4fd3c94aa8
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-19 11:07:27 -0400

    Update ResetPassword.jsx

--- src/components/login/ResetPassword.jsx
+++ src/components/login/ResetPassword.jsx
@@ -7,13 +7,8 @@ import Layout from "../Layout.jsx";
 import FormField from "../common/FormField.jsx";
 import BackArrow from "../common/BackArrow";
 
-let backArrow = <BackArrow href="/login/" native={true} />
-const LayoutSlug = function(p) {
-			return (
-				<Layout title="Reset Your Password" headerLeft={backArrow}>{this.props.children}</Layout>
-			)
 
-}
+
 export default class ResetPassword extends Component {
 	constructor(props) {
 		super();
@@ -51,30 +46,27 @@ export default class ResetPassword extends Component {
 		fetch(sendPackage,"/endpoints/reset-password/", "POST", this.successHandler );
 	}
 
-	componentWillMount() {
-		console.log(this.state.reset_token);
-		if(this.props.isLoggedIn) {route('/',true);}
-	}
 	render(props,state) {
+		let title = "Reset Password";
     if(this.state.sent && state.status === "reset_success") {
       return (
-				<LayoutSlug>
-        <p>
-        Your password has been successfully reset. <br/>
-        <a  native href="/login/">Login</a>
-				</p>
-					</LayoutSlug>
+				<Layout title={title}>
+					<p>
+					Your password has been successfully reset. <br/>
+					<a  native href="/login/">Login</a>
+					</p>
+				</Layout>
       )
     }
 
     if(!state.reset_token) {
       return (
-				<LayoutSlug>
-        <p>
-          We can't verify that you requested a password reset. <br/>
-          <a native href="/forgot-password/">Try Again</a>
-        </p>
-					</LayoutSlug>
+				<Layout title={title}> >
+					<p>
+						We can&rsquo;t verify that you requested a password reset. <br/>
+						<a native href="/forgot-password/">Try Again</a>
+					</p>
+				</Layout>
       )
     }
 
@@ -82,41 +74,39 @@ export default class ResetPassword extends Component {
 
 
 		return(
-			<LayoutSlug>
-			<form onSubmit={this.submitForm.bind(this)}>
-        <p>Enter the email address associated with your account and a new password.</p>
-				<FormField
-					labelShort={"email"}
-					value={state.email}
-					required={true}
-					label={"Email Address"}
-					onInput={linkstate(this, 'email')}
-					type={"email"}
-				/>
-        <FormField
-					labelShort={"password"}
-					value={state.password}
-					required={true}
-					label={"New Password"}
-					onInput={linkstate(this, 'password')}
-					type={"password"}
-				/>
-        <FormField
-					labelShort={"password_2"}
-					value={state.password_2}
-					required={true}
-					label={"Confirm Your New Password"}
-					onInput={linkstate(this, 'password_2')}
-					type={"password"}
-				/>
+			<Layout title={title} headerLeft={<BackArrow href="/login/" native={true} />}>
+				<form onSubmit={this.submitForm.bind(this)}>
+					<p>Enter the email address associated with your account and a new password.</p>
+					<FormField
+						labelShort={"email"}
+						value={state.email}
+						required={true}
+						label={"Email Address"}
+						onInput={linkstate(this, 'email')}
+						type={"email"}
+					/>
+					<FormField
+						labelShort={"password"}
+						value={state.password}
+						required={true}
+						label={"New Password"}
+						onInput={linkstate(this, 'password')}
+						type={"password"}
+					/>
+					<FormField
+						labelShort={"password_2"}
+						value={state.password_2}
+						required={true}
+						label={"Confirm Your New Password"}
+						onInput={linkstate(this, 'password_2')}
+						type={"password"}
+					/>
 
-				<button type="submit" disabled={!this.state.email || !this.state.password || !this.state.password_2}>Submit</button>
-				<br/><br/>
-				<a native href="/login/">Go to Login Page</a>
-
-
-			</form>
-				</LayoutSlug>
+					<button type="submit" disabled={!this.state.email || !this.state.password || !this.state.password_2}>Submit</button>
+					<br/><br/>
+					<a native href="/login/">Go to Login Page</a>
+				</form>
+			</Layout>
 
 		)
 
