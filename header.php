4111b7c7749a8b56583ca2865ef0b9c96c842bf2
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-20 09:52:23 -0400

    Update header.php

--- header.php
+++ header.php
@@ -13,7 +13,12 @@ define("ADMIN_EMAIL", $site_specs['admin_email']);
 define("SITE_URL",$site_specs['site_url']);
 
 if($need_to_install) {
-  die('<a href="install.php">Install Plantdaddy</a>');
+	$info_box_content = '
+	  <p>Plantdaddy&rsquo;s database needs to be install. Make sure you have the information filled out in <code>database_info.php</code> filled out correctly</p>
+  <a class="button" href="install.php">Install now</a>
+		'
+		include "info_box.php"
+		die();
 }
 if(file_exists($_SERVER['DOCUMENT_ROOT']."/global_functions")) {
   $dir = new DirectoryIterator($_SERVER['DOCUMENT_ROOT']."/global_functions");
