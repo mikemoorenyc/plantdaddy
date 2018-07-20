3f9a602ad20902c2a8e923f4a2b913d173b1a947
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-20 13:41:06 -0400

    Update UserContainer.js

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -1,17 +1,57 @@
 import { Container } from 'unstated';
+import fetch from "unfetched;
+import { route } from 'preact-router';
 
 export default class UserContainer extends Container {
   state = {
     isLoggedIn: (INITINFO.isLoggedIn === true) ? true : false,
     login_noonce: INITINFO.login_noonce,
-    user: INITINFO.userProfile
+    user: INITINFO.userProfile,
+		login_error: {}
   };
+	
+	loginUser(e) {
+		e.preventDefault();
+		if(!e.target.elements) {
+			return false;
+		}
+		let password = e.target.elements['password'];
+		let email = e.target.elements['email'];
+		if(!email || !password) {
+			this.setState({login_error:"required_missing"});
+			return false;
+		}
+		let request = {
+			email: email,
+			password: password,
+			login_noonce: this.state.login_noonce
+		}
+		
+		async function resultGetter() {
+			let result = await fetch(state,"/endpoints/login-user/", "POST" ,request);
+			if(!result.success) {
+				this.setState({
+					login_error: {
+						errored: true,
+						reason: result.error_code
+					}
+				});
+				return false;
+			}
+			this.setState({
+				user: result.user,
+				isLoggedIn : true
+			});
+			setTimeout(function(){
+				route("/",true)
+			}, 2000);
+			
+		}.bind(this)
+		
+	}
 
-  recieveNewStateItem(key, state) {
-	   let sObj = {};
-     sObj[key] = state;
-     this.setState(sObj);
- };
+	
 
+  
 
 }
