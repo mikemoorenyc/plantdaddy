230d0617dfdd36e64691410f0e10428e31242e7d
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-20 13:44:45 -0400

    Update index.js

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,9 +2,13 @@ import { h, Component } from 'preact';
 import { route } from 'preact-router';
 import fetch from "../../util/endpointFetch.js";
 import linkstate from "linkstate";
-import Layout from "../Layout.jsx";
 
 import FormField from "../common/FormField.jsx";
+import Layout from "../Layout.jsx";
+
+import UserContainer from "../../containers/UserContainer";
+import {Subscribe} from "unstated";
+
 
 
 export default class Login extends Component {
@@ -16,47 +20,19 @@ export default class Login extends Component {
 			email: '',
 			first_name: null,
 		}
-		this.responseHandler = this.responseHandler.bind(this);
+
   }
 
-	responseHandler(r) {
-		if(!r.success) {
-			//ERROR HANDLINE
-		}
-		this.setState({
-				loggedIn: true,
-				first_name: r.data.user.first_name
-		});
-		this.props.UserContainer.recieveNewStateItem('isLoggedIn',true);
-
-		this.props.UserContainer.recieveNewStateItem('user', r.data.user);
-		setTimeout(function(){
-			route("/",true)
-		}, 2000);
-	}
-	submitForm(e) {
-		e.preventDefault();
-		if(!this.state.password || !this.state.email) {return false;}
-		let state = this.state;
-    state.login_noonce = this.props.login_noonce;
-
-		fetch(state,"/endpoints/login-user/", "POST" ,this.responseHandler);
-	}
-
-
-
-  render(props,state) {
-		let title = "Log into PlantDaddy";
-		if(props.UserContainer.state.isLoggedIn) {
-			return(
-				<Layout title={title}><div>You&rsquo;re logged in, {this.state.first_name}</div></Layout>
-			)
-		}
+  render(props,state) {		
     let disabled = (state.email && state.password) ? false : true;
-
     return (
-		<Layout title={title}>
-      <form onSubmit={this.submitForm.bind(this)}>
+		<Layout title={"Log into PlantDaddy}>
+			<Subscribe to={[UserContainer]}>
+			{function(user) {
+				if(user.isLoggedIn) {
+					return <div>You&rsquo;re logged in, {this.state.first_name}</div>
+				}
+				return <form value="" onSubmit={()=>(linkstate(this, "password"),user.loginUser)}>
 				<FormField
 					labelShort={"email"}
 					value={state.email}
@@ -75,9 +51,18 @@ export default class Login extends Component {
 				/>
 				<button disabled={disabled}>Log In</button>
         <br/><br/>
-        <a href="/create-account/">Create a new account</a><br/>
-	    <a href="/forgot-password/">Forget your password?</a>
+        
 			</form>
+				
+			
+			
+			
+			}.bind(this)}
+			
+			</Subscribe>
+      
+			<a href="/create-account/">Create a new account</a><br/>
+	    <a href="/forgot-password/">Forget your password?</a>
 	</Layout>
     )
   }
