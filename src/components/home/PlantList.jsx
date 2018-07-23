90e9169c9db048a0cd00517ebd83121238bf51ee
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-23 15:44:59 -0400

    Update PlantList.jsx

--- src/components/home/PlantList.jsx
+++ src/components/home/PlantList.jsx
@@ -1,1 +1,21 @@
+import {h} from "preact";
+import {Subscribe} from "unstated";
 
+import PlantContainer from "../../containers/PlantContainer";
+
+export default function PlantList(p) {
+	return <Subscribe to={[PlantContainer]}>
+			{function(plants){	
+				if(!plants.loadError && !plants.plants) {
+					plants.getPlants();
+					return <p>Loading</p>
+				}
+				if(plants.loadError) {
+					return <p>LoadError</p>
+				}
+				return <div>Plant List</div>
+				
+			}.bind(this)}
+		</Subscribe>
+	
+}
