bedcc746d96e6f8f03279ef406d4ce16732c9ccf
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-23 15:55:33 -0400

    Update PlantContainer.js

--- src/containers/PlantContainer.js
+++ src/containers/PlantContainer.js
@@ -1,1 +1,34 @@
+import { Container } from 'unstated';
+import fetch from "unfetched";
 
+
+export default class PlantContainer extends Container {
+	state = {
+		plants:null,
+		loadError: false
+	}
+
+	getPlants() {
+		async function plantGetter() {
+			let plants = await fetch(state,"/endpoints/plants/", "GET" );
+			if(!plants.success) {
+				this.setState({
+					loadError: true
+				});
+				return false;
+			}
+			this.setState({
+				loadError: false,
+				plants: plants.data.plants
+			});
+			
+		}.bind(this);
+		plantGetter();
+		
+	}
+
+	
+	
+	
+	
+}
