baeec4571586b9b42864daa54507610ab46a36a5
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-23 16:14:58 -0400

    Update PlantList.jsx

--- src/components/home/PlantList.jsx
+++ src/components/home/PlantList.jsx
@@ -1,21 +1,47 @@
-import {h} from "preact";
+import {h, Component} from "preact";
 import {Subscribe} from "unstated";
 
 import PlantContainer from "../../containers/PlantContainer";
 
-export default function PlantList(p) {
-	return <Subscribe to={[PlantContainer]}>
-			{function(plants){	
-				if(!plants.loadError && !plants.plants) {
-					plants.getPlants();
-					return <p>Loading</p>
-				}
-				if(plants.loadError) {
-					return <p>LoadError</p>
-				}
-				return <div>Plant List</div>
+export default class PlantList extends Component {
+	constructor() {
+		super();
+		this.state = {
+			status: "loading",
+			plants: null	
+		}
+	}
+	
+	render(p,s) {
+		if(s.status == "loading") {
+			return <div>Loading</div>
+		}
+		if(s.status === "error" ) {
+			return <div>Load Error</div>
+		}
+		
+		if(!s.plants.length) {
+		return	<a href="/add-plant/">Add a plant</a>
+		}
+		
+		return <div class="plant-list"><Subscribe to={[PlantContainer]}>
+			{function(pc) {
+				let plist = s.plants.map(function(e,i) {
+					pc.getPlant(e);
+					return <div>{e.name}</div>
+				})
+				
+				return {plist}
 				
 			}.bind(this)}
+			
+			
+			
 		</Subscribe>
+		</div>
+		
+	}
+	
 	
 }
+
