3b91d347afe6ec3c762fc610cc2af75675d1d6b5
Mike Moore <tigerhollowpoint@gmail.com>
2018-07-24 16:55:25 -0400

    Update ProfileImage.jsx

--- src/components/common/ProfileImage.jsx
+++ src/components/common/ProfileImage.jsx
@@ -1,10 +1,11 @@
 import {h} from "preact";
 
-export default function({user}) {
+export default function({user, size}) {
+	let circleDiameter = parseInt(size) || 100;
   let color = user.color;
   let interior = (user.photo_url)? <img src={user.photo_url} alt={user.first_name} /> : <span>{p.user.first_name.charAt(0)}</span>;
   return(
-    <div style={{backgroundColor: color}} >
+    <div style={{backgroundColor: color, width: circleDiameter, height: circleDiameter}} >
       {interior}
     </div>
   )
