37183ee5b1e020716aa257b530e0c4ea5bbf6f7a
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-01 11:44:53 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,35 +1,44 @@
-import { h } from 'preact';
+import { Component, h } from 'preact';
 import { Subscribe } from 'unstated';
 import UserContainer from "../containers/UserContainer.js";
 import LayoutContainer from "../containers/LayoutContainer.js";
 import MainMenu from "./MainMenu.jsx";
 
-export default function(p) {
-  let title = p.title || "PlantDaddy" ;
-  return (
-    <Subscribe to={[UserContainer,LayoutContainer]}>
-      {function(user,layout) {
-        let menu = (user.state.isLoggedIn) ? <MainMenu /> : null;
-        return(
-          <div id="app" className={(layout.state.menuOpen) ? "menu-open" : null}>
-            {menu}
-            <div id="main-section">
-              <div class="header">
-                <div class="left-side">
-                  {p.headerLeft}
-                </div>
-                <h1>{title}</h1>
-                <div class="right-side">
-                  {p.headerRight}
-                </div>
-              </div>
-              <div class="content">
-                {p.children}
-              </div>
-            </div>
-          </div>
-        )
-      }}
-    </Subscribe>
-  )
+export default class Layout extends Component {
+	
+	componentDidMount() {
+		let title = (this.props.title) ? this.props.title+" | PlantDaddy" : "PlantDaddy";
+		document.title = title;
+	}
+	render(p,s) {
+		let title = p.title || "PlantDaddy" ;
+		return (
+			<Subscribe to={[UserContainer,LayoutContainer]}>
+				{function(user,layout) {
+					let menu = (user.state.isLoggedIn) ? <MainMenu /> : null;
+					return(
+						<div id="app" className={(layout.state.menuOpen) ? "menu-open" : null}>
+							{menu}
+							<div id="main-section">
+								<div class="header">
+									<div class="left-side">
+										{p.headerLeft}
+									</div>
+									<h1>{title}</h1>
+									<div class="right-side">
+										{p.headerRight}
+									</div>
+								</div>
+								<div class="content">
+									{p.children}
+								</div>
+							</div>
+						</div>
+					)
+				}}
+			</Subscribe>
+		)		
+	}
+	
 }
+
