87d7b38e7c9fac92699880f19e5892133310c015
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-16 11:30:18 -0400

    Update get_plant_by_id.php

--- global_functions/get_plant_by_id.php
+++ global_functions/get_plant_by_id.php
@@ -1,1 +1,23 @@
+<?php
+function get_plant_by_id($id) {
+	if(!$id) {
+		return false;
+	}
+	$plant =  get_items(array(
+		"limit" => 1,
+		"table" => "plant",
+		"selector_key" => "id",
+		"selector_value" => $id
+	));
+	if (!$plant) {
+		return false;
+	}
+	
+	return $plant;
+	
+	
+}
 
+
+
+?>
