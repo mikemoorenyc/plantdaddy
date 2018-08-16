f60e2647b87b4edf3e4de182dccfa3331ea8d88c
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-16 10:09:45 -0400

    Update get-accounts.php

--- endpoints/accounts/get-accounts.php
+++ endpoints/accounts/get-accounts.php
@@ -1,1 +1,31 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] ."/header.php";
+require_once $_SERVER['DOCUMENT_ROOT'] ."/endpoints/endpoint-header.php";
+if(!$_SESSION['logged_in']) {
+	errorResponse();
+}
 
+if(!$response['id']) {
+	errorResponse();
+}
+
+//Get User
+$db_user = get_user_by_id($response['id']);
+if(!$db_user) {
+	errorResponse();
+}
+
+//Check if User Needs Updating
+$needs_updating = false;
+$keys = ["id", "photo_id", "email", "first_name" , "color","telephone"];
+foreach($keys as $k) {
+	if($db_user[$k] !== $response[$k] {
+		$needs_updating = true;
+	}
+}
+if(!$needs_updating) {
+	errorResponse(304);
+}
+
+echo json_encode($db_user);
+die();
