d5112c5b764afa47d9f80eac6eebfa8915d7cd1e
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-16 09:58:20 -0400

    Update Accounts.js

--- src/containers/Accounts.js
+++ src/containers/Accounts.js
@@ -1,1 +1,45 @@
+import { Container } from 'unstated';
+import fetch from "../util/endpointFetch";
 
+export default class UserContainer extends Container {
+	state = {
+		accounts: {},
+		fetchError: null	
+	};
+	fetchError(statement) {
+		this.setState({
+			fetchError: statement
+		});
+		setTimeout(function(){
+			this.setState({fetchError:null});
+		}.bind(this),5000);
+		
+	}
+	updateAccount(response) {
+		switch(response) {
+    case (response.status === 304):
+        return null;
+        break;
+    case (response.status > 399):
+        this.fetchError('Could not find account');
+				return null
+        break;
+    default:
+				let accounts = this.state.accounts;
+				accounts[response.data.id] = response.data;
+        this.setState({accounts: accounts});
+		}
+	}
+	getAccount(id) {
+		if(!id || isNaN(id)) {fetchError('Could not find account');return false;}
+		
+		let currentData = this.state.accounts[id] || {};
+		
+		fetch(currentData,"/endpoints/accounts/${id}/", "GET" , this.fetchError); 
+	}
+
+	
+	
+	
+	
+}
