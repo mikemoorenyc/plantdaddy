d55da927527a5a49f84d8e434aa1d005c2602362
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-17 13:19:04 -0400

    Update update-plant.php

--- endpoints/plants/update-plant.php
+++ endpoints/plants/update-plant.php
@@ -1,1 +1,13 @@
+<?php
 
+require_once $_SERVER['DOCUMENT_ROOT'] ."/header.php";
+require_once $_SERVER['DOCUMENT_ROOT'] ."/endpoints/endpoint-header.php";
+
+if(!$_SESSION['logged_in']) {
+	errorResponse();
+}
+
+
+
+
+?>
