137e769451ba79559464b45cf1e940d4bea35642
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-17 11:08:32 -0400

    Update create-plant.php

--- endpoints/plants/create-plant.php
+++ endpoints/plants/create-plant.php
@@ -12,12 +12,19 @@ if(!empty($required_diff)) {
 	errorResponse(400, "empty_fields ".implode(",",$required_diff));
 }
 
-//CHECK IF EMAIL EXISTS
-$get_plant =  "SELECT title FROM plants WHERE `title` = '".$db_conn->real_escape_string($response['title'])."' LIMIT 1";
-$plane = $db_conn->query($get_plant);
-if($plant->num_rows > 0) {
-	errorResponse(406, "duplicate name");
+//CHECK IF Title EXISTS
+$all_plants =  get_plants();
+if(!empty($all_plants)) {
+	$matches = array_filter($all_plants,function($p) {
+		return strtolower($response['title']) === strtolower($p['title']);
+	});
+	if(!empty($matches)) {
+		errorResponse(400, "title match");
+	}
 }
+
+
+
 $insert_fields = array(
 	"title" => $response['title'],
 	"watering_frequency" => $response["watering_frequency"],
