080cfc897d81b20aae464159d280e40e1e393f01
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-17 14:59:42 -0400

    Update update-plant.php

--- endpoints/plants/update-plant.php
+++ endpoints/plants/update-plant.php
@@ -7,7 +7,31 @@ if(!$_SESSION['logged_in']) {
 	errorResponse();
 }
 
+$plant = get_items(array(
+	"selector_key" => "id",
+	"selector_value" => $response['id'],
+	"limit" => 1,
+	"table" => "plants"
+));
 
+if(!$plant) {
+	errorResponse(404);
+}
 
+$update_keys = [];
+
+foreach($response as $k => $v) {
+	if($v) !== $plant[$k]) {
+		$update_keys[] = $k;
+	}
+}
+
+if(in_array("title", $update_keys) && !$response['title']) {
+	errorResponse(400, "no title");
+}
+
+if(plant_title_exists($response['title']) {
+	errorResponse(400,"title exists");
+}
 
 ?>
