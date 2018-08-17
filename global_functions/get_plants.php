51da8169e05a878e67e892afcbbf573bb1d3d191
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-17 11:03:08 -0400

    Update get_plants.php

--- global_functions/get_plants.php
+++ global_functions/get_plants.php
@@ -1,6 +1,19 @@
 <?php
 
+function get_plants($amount, $columns) {
+	$plants = get_items(
+		array(
+			"limit" => $amount ?: 999,
+			"fields" => $columns ?: null,
+			"table" => "plants"
+		)
+	);
+	if(!$plants) {
+		return [];
+	}
+	return $plants;
 
+}
 
 
 ?>
