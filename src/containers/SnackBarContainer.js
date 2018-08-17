97facbc4acea11e9959c840b1c1cb7b08aaaa270
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-17 12:00:26 -0400

    Update SnackBarContainer.js

--- src/containers/SnackBarContainer.js
+++ src/containers/SnackBarContainer.js
@@ -1,1 +1,26 @@
 import { Container } from 'unstated';
+
+
+export default class SnackBarContainer extends Container {
+	state = {
+		snackbars: []
+	}
+
+	createSnackBar(obj) {
+		if(!obj.text) {
+			return false;
+		}
+		let snackbars = this.state.snackbars.slice();
+		let stamp = Date.now();
+		snackbars.push({
+			text: obj.text,
+			kind: obj.kind || "neutral",
+			stamp: stamp
+		})
+		this.setState({snackbars: snackbars});
+	}
+	
+	
+	
+	
+}
