76dcb1b3b6693a7d22aff36470a00261eec96185
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-17 16:14:46 -0400

    Update update-plant.php

--- endpoints/plants/update-plant.php
+++ endpoints/plants/update-plant.php
@@ -20,15 +20,15 @@ if(!$plant) {
 
 $to_update = [];
 
+$allowed_values = ["title","watering_frequency"];
+
 foreach($response as $k => $v) {
-	if($k === "photo_data") {
-		continue;
-	}
-	if($v !== $plant[$k]) {
+	if($v !== $plant[$k] && in_array($k, $allowed_values)) {
 		$to_update[$k] = $k;
 	}
 }
 
+//MAKE SURE TITLE IS OK
 if(in_array("title", array_keys($to_update)) && !$response['title']) {
 	errorResponse(400, "no title");
 }
@@ -36,6 +36,30 @@ if(in_array("title", array_keys($to_update)) && !$response['title']) {
 if(plant_title_exists($response['title'])) {
 	errorResponse(400,"title exists");
 }
+
+//MAKE UPDATE PACKAGE
+if(!empty($response['photo_data']) && $response['photo_data'] !== get_photo_by_id($plant['photo_id']) ) {
+	$photo_id = upload_image($to_update['photo_data'], "plant_img",);
+	if(!$photo_id) {
+		errorResponse(500,"image_error");
+	}
+	$to_update['photo_id'] = $photo_id;
+}
+if(empty($to_update)) {
+	errorResponse(304, "not_modified");
+}
+
+$updated_item = update_item(array(
+	"table" => "plants",
+	"selector_value" => $plant['id'],
+	"selector_key" => "id",
+	"update_array" => $to_update
+));
+if(!$updated_item) {
+	errorResponse(500, "not_updated");
+}
+echo json_encode(get_plant_by_id($updated_item['id']));
+die();
 
 
 
