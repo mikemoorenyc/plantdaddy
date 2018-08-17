316a50ea050a6eca00748bee425ca62935d58435
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-17 10:34:56 -0400

    Update and rename CreatePlant.jsx to EditPlant.jsx

---
+++ src/components/plants/EditPlant.jsx
@@ -0,0 +1,29 @@
+
+import { h, Component } from 'preact';
+import linkstate from "linkstate"
+import { route } from 'preact-router';
+import fetch from "unfetch";
+import Layout from "../Layout.jsx";
+
+import FormSection from "../common/FormField.jsx";
+import BackArrow from "../common/BackArrow.jsx";
+
+export default class EditPlant extends Component {
+	
+	constructor(props) {
+		super();
+		let plant = (props.pc.state.plants[props.id]) ? props.pc.state.plants[props.id]: 
+			{title: "", watering_frequency:null,photo_url:"",id:null}
+		this.state = {
+			plant: plant,
+			loading: null
+		}
+		
+	}
+	
+	
+	render(props,state) {
+		
+		
+	}
+}
