1aa4a748f0b1965de8bb49afdfb9b283a6c76f70
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-17 11:52:27 -0400

    Update Snackbar.jsx

--- src/components/Snackbar.jsx
+++ src/components/Snackbar.jsx
@@ -1,1 +1,20 @@
 
+import { h } from 'preact';
+
+
+export default function(props) {
+	if(props.snackbars.length === 0) {
+		return false;
+	}
+	let snackbars = props.snackbars.sort(function(a,b){
+		return a.created - b.created;
+	});
+	return snackbars.map(function(e,i){
+		let classes = "snackbar ${e.kind}";
+		return <div class={classes} >{e.text}</div>
+	});
+	
+	
+	
+	
+}
