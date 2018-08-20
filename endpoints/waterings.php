7d2289a082d906cebdea9cb3392f6266f3fb7188
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-20 15:54:11 -0400

    Update waterings.php

--- endpoints/waterings.php
+++ endpoints/waterings.php
@@ -1,6 +1,18 @@
 <?php
 
-
+$method = $_SERVER['REQUEST_METHOD'];
+if(!$_SESSION['logged_in']) {
+	errorResponse(401) ;
+}
+switch($method) {
+	case 'POST':
+		require 'waterings/create-watering.php';
+		break;
+	default:
+		require "waterings/get-waterings.php";
+		break;
+}
+die()
 
 
 ?>
