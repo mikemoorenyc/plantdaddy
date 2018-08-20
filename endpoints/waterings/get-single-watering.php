1d7a78dd0101b85d0b47db94711d6f23a19ecdb2
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-20 16:11:49 -0400

    Update get-single-watering.php

--- endpoints/waterings/get-single-watering.php
+++ endpoints/waterings/get-single-watering.php
@@ -1,4 +1,16 @@
 <?php
 $w_id = intval($url_sections[1]);
 
+$watering = get_items(array(
+	"table" => "waterings",
+	"selector_key" => "id",
+	"selector_value" => $w_id,
+	"limit" => 1
+));
+if(!$waterings) {
+	errorResponse(404);
+	die();
+}
 
+echo json_encode(array("data" => $watering));
+die();
