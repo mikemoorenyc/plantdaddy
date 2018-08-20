76082be313df22e77e1e042769051057e70610bb
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-20 10:36:59 -0400

    Update get-plants.php

--- endpoints/plants/get-plants.php
+++ endpoints/plants/get-plants.php
@@ -1,32 +1,45 @@
 <?php
 
-if(!$url_sections[1]) {
-	if(!$_GET['page'] || !is_int(intval($_GET['page'])) || $_GET['page'] < 1) {
-		$page = 0;
-	} else {
-		$page = intval($_GET['page']) - 1;
+if($url_sections[1]) {
+	if(!is_numeric($url_sections[1])) {
+		errorResponse();
 	}
+	$id = intval($url_sections[1]);
+	$plant = get_plant_by_id($id);
+
+	if(!$plant) {
+		errorResponse(404);
+	}
+
+	echo json_encode(array("data" => $plant));
+	die();
+}
+
+if(!$_GET['page'] || !is_numeric($_GET['page']) || intval($_GET['page']) < 1) {
+	$page = 1;
+} else {
+	$page = intval($_GET['page']);
+}
 	$plants = get_items(array(
 		"table" => "plants",
 		"limit" => 25,
-		"offset" => $page * 25		
+		"offset" => ($page -1) * 25		
 	));
 	if(!$plants) {
-		echo json_encode(array());
-		die();
+		$plants = [];
 	}
-	echo json_encode($plants);
+
+	$row = $db_conn->query( "select count(id) as num_rows from table"  );
+	$total = $row->num_rows;
+
+	$total_pages = ($total < 25) ? 1 : (int) ($$total /25);
+	$return_package = array(
+		"page" => $page,
+		"per_page" => 25,
+		"total" => $total,
+		"total_pages" => $total_pages,
+		"data" => $plants
+	);
+echo json_encode($return_package);
 	die();
-}
-if(!is_int($url_sections[1])) {
-	errorResponse();
-}
-$plant = get_plant_by_id($url_sections[1]);
-
-if(!$plant) {
-	errorResponse(404);
-}
-
-echo json_encode(array("data" => $plant));
-die();
 ?>
