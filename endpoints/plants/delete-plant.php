799539d55e1ffb1482449595ed75b2ebb2699087
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-20 09:50:56 -0400

    Update delete-plant.php

--- endpoints/plants/delete-plant.php
+++ endpoints/plants/delete-plant.php
@@ -1,18 +1,25 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] ."/header.php";
 require_once $_SERVER['DOCUMENT_ROOT'] ."/endpoints/endpoint-header.php";
-if(!$_SESSION['logged_in']) {
-	errorResponse(401);
-}
-if(!$response['id']) {
+
+if(!$_SESSION['logged_in'] || !$url_sections[1] || !is_int($url_sections[1])) {
 	errorResponse();
 }
+
 //GET PLANT
-$plant = get_plant_by_id($response['id']);
+$plant = get_plant_by_id($url_sections[1]);
 
 if(!$plant) {
 	errorResponse(404);
 }
+$deleted_plant = delete_item("plants", $plant['id']);
+
+if(!$deleted_plant) {
+	errorResponse(500);
+}
+http_response_code(204);
+
+
 
 die();
 ?>
