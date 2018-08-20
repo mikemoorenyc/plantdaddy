db57658134d4b4d1364fe70af2a6e02e5b69eae5
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-20 10:03:15 -0400

    Update get-plants.php

--- endpoints/plants/get-plants.php
+++ endpoints/plants/get-plants.php
@@ -1,1 +1,18 @@
+<?php
 
+if(!$url_sections[1]) {
+	echo json_encode(get_plants());
+	die();
+}
+if(!is_int($url_sections[1])) {
+	errorResponse();
+}
+$plant = get_plant_by_id($url_sections[1]);
+
+if(!$plant) {
+	errorResponse(404);
+}
+
+echo json_encode($plant);
+die();
+?>
