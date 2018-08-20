c83ae64a63f1c95dbb01c032943906859c2c1901
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-20 16:13:32 -0400

    Update get-waterings.php

--- endpoints/waterings/get-waterings.php
+++ endpoints/waterings/get-waterings.php
@@ -1,7 +1,11 @@
 <?php
 
-if(is_numeric($url_sections[1]) {
+if(is_numeric($url_sections[1])) {
 	require "get-single-watering.php";
+	die();
+}
+if($url_sections[1] === "plant") {
+	require "get-plant-waterings.php";
 	die();
 }
 
