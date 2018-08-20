01da5a51c2e9c841d3b075c7028813b77769639c
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-20 10:40:51 -0400

    Update delete-plant.php

--- endpoints/plants/delete-plant.php
+++ endpoints/plants/delete-plant.php
@@ -2,12 +2,12 @@
 require_once $_SERVER['DOCUMENT_ROOT'] ."/header.php";
 require_once $_SERVER['DOCUMENT_ROOT'] ."/endpoints/endpoint-header.php";
 
-if( !$url_sections[1] || !is_int($url_sections[1])) {
+if( !$url_sections[1] || !is_numeric($url_sections[1])) {
 	errorResponse();
 }
 
 //GET PLANT
-$plant = get_plant_by_id($url_sections[1]);
+$plant = get_plant_by_id(intval($url_sections[1]));
 
 if(!$plant) {
 	errorResponse(404);
