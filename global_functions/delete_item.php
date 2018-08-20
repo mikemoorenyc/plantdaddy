3a3b4ff54b95b0ffed0e48ece41c7af40e8f14ea
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-20 09:49:07 -0400

    Update delete_item.php

--- global_functions/delete_item.php
+++ global_functions/delete_item.php
@@ -1,6 +1,21 @@
-<?
+<?php
+
+function delete_item($table,$id) {
+	if(!$table ||!$id || !is_int($id)) {
+		return false;
+	}
+	global $db_conn;
+	$safe_table = $db_conn->real_escape_string($table);
+	$safe_id = intval($id);
+	$sql =  "DELETE FROM $safe_table WHERE selector=$safe_id";
+  $deleted_item =  mysqli_query($db_conn, $sql);
+	if(!$deleted_item) {
+		return false;
+	}
+	return true;
+}
 
 
 
 
-?
+?>
