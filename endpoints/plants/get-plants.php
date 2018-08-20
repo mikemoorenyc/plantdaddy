7260d79f3455a9081e797dd38e393129fdf022f3
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-20 10:15:35 -0400

    Update get-plants.php

--- endpoints/plants/get-plants.php
+++ endpoints/plants/get-plants.php
@@ -1,7 +1,21 @@
 <?php
 
 if(!$url_sections[1]) {
-	echo json_encode(get_plants());
+	if(!$_GET['page'] || !is_int(intval($_GET['page'])) || $_GET['page'] < 1) {
+		$page = 0;
+	} else {
+		$page = intval($_GET['page']) - 1;
+	}
+	$plants = get_items(array(
+		"table" => "plants",
+		"limit" => 25,
+		"offset" => $page * 25		
+	));
+	if(!$plants) {
+		echo json_encode(array());
+		die();
+	}
+	echo json_encode($plants);
 	die();
 }
 if(!is_int($url_sections[1])) {
@@ -13,6 +27,6 @@ if(!$plant) {
 	errorResponse(404);
 }
 
-echo json_encode($plant);
+echo json_encode(array("data" => $plant));
 die();
 ?>
