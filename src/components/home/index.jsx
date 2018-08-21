e21254bce81d05495e26a6e4f8e829e75898a01f
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-21 16:23:13 -0400

    Update index.jsx

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -5,17 +5,19 @@ import {Subscribe} from "unstated";
 
 import LayoutContainer from "../../containers/LayoutContainer";
 import UserContainer from "../../containers/UserContainer";
+import PlantContainer from "../../containers/PlantContainer";
+import AccountContainer from "../../containers/Accounts";
 
 import ProfileImage from "../common/ProfileImage";
 import PlanList from "./PlantList.jsx";
 
 export default function(props) {
   return(
-		<Subscribe to={[LayoutContainer,UserContainer]}>
-			{function(layout,user) {
+		<Subscribe to={[LayoutContainer,UserContainer,PlantContainer,AccountConainer]}>
+			{function(layout,user,plant,account) {
 				return(
 					<Layout headerLeft={<button onClick={layout.toggleMenu}><ProfileImage user={user.state.user} /></button>}>
-						<PlantList />
+						<PlantList pc={plant} ac={account} />
 					</Layout>
 				)
 			}}
