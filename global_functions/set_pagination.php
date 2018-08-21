ddd54259eb67f5739ef75ea5c20bdf46151c9397
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-21 09:28:00 -0400

    Update set_pagination.php

--- global_functions/set_pagination.php
+++ global_functions/set_pagination.php
@@ -1,1 +1,26 @@
-
+<?php
+function set_pagination($page, $per_page) {
+	if(intval($per_page) === -1) {
+		return array(
+			"limit" => 9999,
+			"offset" => 0,
+			"per_page" => 9999,
+			"page" => 1
+		);
+	}
+	$limit = intval($per_page);
+	if(!$limit) {
+		$limit = 25;
+	}
+	$offset = intval($page); 
+	if(!$offset) {
+		$offset = 1;
+	}
+	return array(
+		"limit" => $limit,
+		"offset" => $limit * ($offset-1),
+		"per_page" => $limit,
+		"page" => $offset
+	);
+	
+}
