071c4e67487cc1ef6c600a83a2412273d55282a2
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-21 09:27:47 -0400

    Update get-plant-waterings.php

--- endpoints/waterings/get-plant-waterings.php
+++ endpoints/waterings/get-plant-waterings.php
@@ -9,15 +9,14 @@ $plant = get_plant_id($plant_id);
 if(!$plant) {
 	errorResponse(404);
 }
-$page =(is_numeric($_GET['page']) && intval($_GET['page']) > 0) ? 0 : intval($_GET['page']);
-$per_page = (is_numeric($_GET['per_page']) && intval($_GET["per_page"]) > 0) ? 25 : intval($_GET['per_page']);
+$pagination = set_pagination($_GET['page'], $_GET['per_page']);
 
 $waterings = get_items(array(
 	"table" => "waterings",
 	"selector_key" => "plant_id",
 	"selector_value" => $plant['id'],
-	"limit" => $per_page,
-	"offset" => ($page -1) * 25	
+	"limit" => $pagination['limit'],
+	"offset" => $pagination['offset']
 ));
 if(!$waterings) {
 	http_response_code(204);
@@ -27,10 +26,10 @@ $row = $db_conn->query( "select count(id) as waterings_total from waterings wher
 $total = $row->waterings_total;
 
 $result_package = array(
-	"page" => $page,
+	"page" => $pagination['page'],
 	"total" => $total,
-	"per_page" => $per_page,
-	"total_pages" => ($total < $per_page) ? 1 : ceil($total / $per_page) 
+	"per_page" => $pagination['per_page'],
+	"total_pages" => ($total < $pagination['per_page']) ? 1 : ceil($total / $pagination['per_page']) 
 );
 $data = array();
 $data['waterings'] = [];
