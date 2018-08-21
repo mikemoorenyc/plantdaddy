1f5a2522d7827127eb3c1135c4a75f08b4d41ac8
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-21 17:02:57 -0400

    Update PlantContainer.js

--- src/containers/PlantContainer.js
+++ src/containers/PlantContainer.js
@@ -1,11 +1,14 @@
 import { Container } from 'unstated';
 import fetch from "unfetched";
 
+import {removeItem} from "../util/array_helpers";
+import apiSettings from "../util/apiSettings";
 
 export default class PlantContainer extends Container {
 	state = {
 		plants: {},
-		fetching: null
+		fetching: [],
+		errored: []
 	}
 
 	getPlant(plant) {
@@ -13,6 +16,43 @@ export default class PlantContainer extends Container {
 		plants[plant.id] = plant;
 		this.setState({plants: plants});		
 	}
+
+	fetchPlant(id,force) {
+		if((this.state.plants[id] && !force) || this.state.fetching.includes(id)) {
+			 return false;
+		}
+		let fetching = this.state.fetching.slice();
+		fetching.push(id);
+		this.setState({fetching: fetching});
+		this.setState({errored: removeItem(id, this.state.errored)});
+		fetch("/endpoints/plants/${id}/",apiSettings({}, "GET"))
+		.then(function(response){
+			this.setState({fetching : removeItem(id, this.state.fetching)});
+			this.setState({fetching: fetching});
+			if(response.status > 200) {
+				throw response.status;
+			}	else {
+					return response.json();
+			}
+			
+		}.bind(this))
+		.then(function(d){
+			this.setState({errored: removeItem(id, this.state.errored)});
+			let plants = this.state.plants;
+			plants[d.id] = d;
+      this.setState({plants: plants});
+		
+		}.bind(this))
+		.catch(function(status){
+			if(status === 304 && this.state.plants[id]) {
+				return false;
+			}
+			let errored = this.state.errored.slice();
+			errored.push(id);
+			this.setState({errored: errored});
+		}.bind(this))
+	}
+
 
 	
 	
